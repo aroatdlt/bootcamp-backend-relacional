7bb25808f31359687ab8700ef114ad9e0ac377ab
aroatdlt <aroatdlt@gmail.com>
2023-03-19 19:26:48 +0100

    add functional test

---
+++ modulo2_apirest/test/BookManager.FunctionalTests/BookTests.cs
@@ -0,0 +1,51 @@
+﻿
+
+using BookManager.Application.Models;
+using BookManager.FunctionalTests.TestSupport;
+using System.Net.Http.Json;
+using System.Text.Json;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+
+namespace BookManager.FunctionalTests;
+
+public class BookTests: IntegrationTest
+{
+    [Fact]
+  public async Task Given_Post_Book_Endpoint_When_Sending_BookId_It_Returns_Book()
+    {
+        //Given
+        //Step 1 send an book 
+
+        var bookPayload = new Book()
+        {
+            authorId = 1,
+            description = "book",
+            publishedOn = new DateTime(2021, 07, 20), 
+            title = "hola"    
+        };
+
+        var addBook = await HttpClient.PostAsJsonAsync($"api/bookmanager/books", bookPayload);
+   
+        if (!addBook.IsSuccessStatusCode)
+        {
+            throw new Exception("Could not add new Book. Test cannot continue");
+        }
+ 
+
+        //When
+        var result = await HttpClient.GetAsync($"api/bookmanager/books");
+
+
+        //Then
+        var json = await result.Content.ReadAsStringAsync();
+        var serializerOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+        var books = (JsonSerializer.Deserialize<IEnumerable<Book>>(json, serializerOptions)
+                       ?? Array.Empty<Book>()).ToList();
+        var insertedBook = books.Find(q => q.title == bookPayload.title);
+
+        insertedBook.Should().NotBeNull();
+
+    }
+}
+
---
+++ modulo2_apirest/test/BookManager.FunctionalTests/TestSupport/IntegrationTest.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace BookManager.FunctionalTests.TestSupport
+{
+    public abstract class IntegrationTest
+    {
+        protected HttpClient HttpClient { get; }
+        protected IntegrationTest()
+        {
+            var server =
+                new TestServer(
+                    new WebHostBuilder()
+                        .UseStartup<Startup>()
+                        .UseEnvironment("Test")
+                        .UseCommonConfiguration()
+                        .ConfigureTestServices(ConfigureTestServices));
+
+            HttpClient = server.CreateClient();
+        }
+
+        protected virtual void ConfigureTestServices(IServiceCollection services)
+        {
+            // to implement in test classes
+        }
+    }
+
+    internal static class WebHostBuilderExtensions
+    {
+        internal static IWebHostBuilder UseCommonConfiguration(this IWebHostBuilder builder)
+        {
+            builder.ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var env = hostingContext.HostingEnvironment;
+
+                config
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                    .AddEnvironmentVariables();
+
+                if (env.IsDevelopment())
+                {
+                    var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
+                    config.AddUserSecrets(appAssembly, optional: true);
+                }
+            });
+
+            return builder;
+        }
+    }
+}
+
+
